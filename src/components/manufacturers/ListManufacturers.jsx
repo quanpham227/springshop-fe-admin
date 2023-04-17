4151e3c4baa21cd546932864ce07909e0b3b2e67
quan-pham <hongquanqtqb@gmail.com>
2023-04-17 20:23:18 +0700

    add Manufacturer form UI

--- src/components/manufacturers/ListManufacturers.jsx
+++ src/components/manufacturers/ListManufacturers.jsx
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import ContentHeader from '../common/ContentHeader';
 import ManufacturerList from './ManufacturerList';
 import withRouter from '../../helpers/withRouter';
+import { Button, Col, Row } from 'antd';
+import ManufacturerForm from './ManufacturerForm';
 
 class ListManufacturers extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            open: false,
+        };
+    }
+
+    onCreate = (values) => {
+        console.log(values);
+    };
     render() {
         const { navigate } = this.props.router;
+        const { open } = this.state;
         return (
             <>
                 <ContentHeader
@@ -13,7 +27,27 @@ class ListManufacturers extends Component {
                     title="List Manufacturers"
                     className="site-page-header"
                 ></ContentHeader>
+                <Row>
+                    <Col md={24}>
+                        <Button
+                            type="primary"
+                            onClick={() => {
+                                this.setState({ ...this.state, open: true });
+                            }}
+                        >
+                            New Manufacturer
+                        </Button>
+                    </Col>
+                </Row>
                 <ManufacturerList />
+
+                <ManufacturerForm
+                    open={open}
+                    onCreate={this.onCreate}
+                    onCancel={() => {
+                        this.setState({ ...this.state, open: false });
+                    }}
+                />
             </>
         );
     }
--- src/components/manufacturers/ManufacturerForm.jsx
+++ src/components/manufacturers/ManufacturerForm.jsx
@@ -1,8 +1,109 @@
-import React, { Component } from 'react';
+import { Button, Divider, Form, Image, Input, Modal, Radio, Upload } from 'antd';
+import React, { Component, createRef } from 'react';
 
 class ManufacturerForm extends Component {
+    form = createRef();
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            manufacturer: { id: '', name: '', logo: '' },
+            previewImage: '',
+            previewVisible: false,
+        };
+    }
+    handlePreview = (file) => {
+        console.log(file);
+        if (file.thumbUrl) {
+            this.setState({ ...this.state, previewImage: file.thumbUrl, previewVisible: true });
+        }
+    };
+
+    handleRemove = (value) => {
+        console.log(value);
+    };
+    normFile = (e) => {
+        if (Array.isArray(e)) {
+            return e;
+        }
+        if (e.fileList.length > 1) {
+            return [e.fileList[1]];
+        }
+        return e && e.fileList;
+    };
+
     render() {
-        return <div>ManufacturerForm</div>;
+        const { open, onCreate, onCancel } = this.props;
+        const { manufacturer } = this.state;
+        return (
+            <Modal
+                open={open}
+                title="New Manufacturer"
+                okText="Create"
+                cancelText="Cancel"
+                onCancel={onCancel}
+                onOk={() => {
+                    this.form.current
+                        .validateFields()
+                        .then((values) => {
+                            this.form.current.resetFields();
+                            onCreate(values);
+                        })
+                        .catch((info) => {
+                            console.log('Validate Failed:', info);
+                        });
+                }}
+            >
+                <Form
+                    ref={this.form}
+                    layout="vertical"
+                    name="form_in_modal"
+                    initialValues={{
+                        modifier: 'public',
+                    }}
+                >
+                    <Form.Item label="Manufacturer ID" name="id" initialValue={manufacturer.id}>
+                        <Input readOnly></Input>
+                    </Form.Item>
+                    <Form.Item
+                        label="Manufacturer name"
+                        name="name"
+                        initialValue={manufacturer.name}
+                        rules={[{ required: true, min: 2 }]}
+                    >
+                        <Input></Input>
+                    </Form.Item>
+                    <Form.Item
+                        label="Logo"
+                        name="logoFile"
+                        initialValue={[{ url: '' }]}
+                        rules={[{ required: true }]}
+                        valuePropName="fileList"
+                        getValueFromEvent={this.normFile}
+                    >
+                        <Upload
+                            listType="picture"
+                            onPreview={this.handlePreview}
+                            onRemove={this.handleRemove}
+                            accept=".jpg,.png,.gif"
+                            maxCount={1}
+                            beforeUpload={() => false}
+                        >
+                            <Button type="primary"> Browse</Button>
+                        </Upload>
+                    </Form.Item>
+                    <Divider></Divider>
+                    {this.state.previewVisible && (
+                        <Image
+                            src={this.state.previewImage}
+                            style={{ width: 200 }}
+                            preview={{ visible: false }}
+                        ></Image>
+                    )}
+                </Form>
+            </Modal>
+        );
     }
 }
 
