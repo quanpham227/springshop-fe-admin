0cc835af84325db4a5cd2afa3c80fd71cf2d691c
quan-pham <hongquanqtqb@gmail.com>
2023-04-24 08:41:53 +0700

    add reducer and actions for product

---
+++ src/redux/actions/ProductAction.jsx
@@ -0,0 +1,50 @@
+import ProductService from '../../services/productService';
+import { COMMON_ERROR_SET, COMMON_LOADING_SET, COMMON_MESSAGE_SET, PRODUCT_SET, PRODUCT__APPEND } from './actionTypes';
+
+export const insertProduct = (product, navigate) => async (dispatch) => {
+    const service = new ProductService();
+
+    try {
+        console.log('Insert Product');
+
+        dispatch({
+            type: COMMON_LOADING_SET,
+            payload: true,
+        });
+
+        const response = await service.insertProduct(product);
+
+        if (response.status === 201) {
+            dispatch({
+                type: PRODUCT_SET,
+                payload: response.data,
+            });
+            // dispatch({
+            //     type: PRODUCT__APPEND,
+            //     payload: response.data,
+            // });
+            dispatch({
+                type: COMMON_MESSAGE_SET,
+                payload: 'Product is saved',
+            });
+        } else {
+            dispatch({
+                type: COMMON_ERROR_SET,
+                payload: response.message,
+            });
+        }
+
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+        dispatch({
+            type: COMMON_ERROR_SET,
+            payload: error.response.data ? error.response.data.message : error.message,
+        });
+    }
+    dispatch({
+        type: COMMON_LOADING_SET,
+        payload: false,
+    });
+    navigate('products/list');
+};
--- src/redux/rootReducer.jsx
+++ src/redux/rootReducer.jsx
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import categoryReducer from './reducers/categoryReducer';
 import commonReducer from './reducers/commonReducer';
 import manufacturerReducer from './reducers/ManufacturerReducer';
+import productReducer from './reducers/productReducer';
 
 const rootReducer = combineReducers({
     categoryReducer: categoryReducer,
     commonReducer,
     manufacturerReducer,
+    productReducer,
 });
 
 export default rootReducer;
